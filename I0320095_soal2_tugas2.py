1ae49446234f4f8318e32291d9e72ddeca99bfa1
Sony Wicaksono <sonywicaksono3232@gmail.com>
2021-03-13 01:58:03 +0700

    soal 2

---
+++ I0320095_soal2_tugas2.py
@@ -0,0 +1,34 @@
+#identitas mahasiswa
+
+nama = "Sony Wicaksono"
+pekerjaan = "Mahasiswa"
+pekerjaan2 = "Store Manager Cafe"
+jurusan = "Teknik Industri"
+angkatan = 2020
+cita_cita = "Konsultan Bidang FnB"
+alamat = "Gulon RT05/RW20, Jebres, Surakarta"
+anak_ke = 3
+ibu = "Sari Diyah"
+ayah = "Narwondo"
+kakakpertama = "Fina Nur Arista"
+kakakkedua = "Exsakti Mayasari"
+Umur = 18
+gol_darah = 'B'
+tinggi = 170
+berat_badan = 62
+ukuran_sepatu = 43
+hobi = "Nontom Film"
+instagram = "@sonnywii"
+
+print("Hai nama saya", nama, "umur saya", Umur)
+print("saya sekarang seorang", pekerjaan, "pada jurusan", jurusan, "angkatan", angkatan)
+print("saat ini saya juga sedang bekerja sebagai", pekerjaan2)
+print("cita-cita saya menjadi seorang", cita_cita)
+print("saya bertempat tinggal di", alamat)
+print("ayah saya bernama", ayah, "dan ibu saya adalah", ibu, "dan saya adalah anak ke", anak_ke)
+print(" saya memiliki 2 kakak yaitu", kakakpertama,"dan", kakakkedua)
+print("golongan darah saya adalah", gol_darah)
+print("tinggi badan saya", tinggi, " dan berat badan saya", berat_badan)
+print("ukuran sepatu saya", ukuran_sepatu)
+print("hobi saya yaitu", hobi)
+print("akun instagram saya adalah", instagram)
