fab6bacd2166458a41b84c581c9fe04f73793dbd
Sony Wicaksono <sonywicaksono3232@gmail.com>
2021-03-13 01:36:10 +0700

    soal 1

---
+++ I0320095_soal1_tugas2.py
@@ -0,0 +1,32 @@
+
+#menghitung luas persegi panjang
+panjang = 15
+lebar = 18
+luas_persegipanjang = panjang * lebar
+print("panjang: ", panjang)
+print("lebar: ", lebar)
+print("luas persegi panjang: ", luas_persegipanjang)
+
+#menghitung luas lingkaran
+r = float(14)
+luas_lingkaran = 3,14*(r ** 2)
+print("r: ", r)
+print("luas lingkaran: ", luas_lingkaran)
+
+#menghitung luas kubus
+s = 10
+luas_permukaan_kubus = 6 * (s ** 2)
+print("s: ", s)
+print("luas permukaan kubus: ", luas_permukaan_kubus)
+
+#menghitung koversi suhu celcius ke farenheit
+C = 65
+F = ((9/5) * C) + 32
+print("C: ", C)
+print("F: ", F)
+
+#menghitung suhu reamur ke kelvin
+R = 48
+K = ((5/4) * R) + 273
+print("R: ", R)
+print("K: ", K)
