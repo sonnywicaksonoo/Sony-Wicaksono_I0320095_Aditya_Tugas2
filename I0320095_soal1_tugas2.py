f4cbcc7cdf975d8571aae0e84c81733ae2e7dbe2
Sony Wicaksono <sonywicaksono3232@gmail.com>
2021-03-13 02:44:29 +0700

    soal 2

--- I0320095_soal1_tugas2.py
+++ I0320095_soal1_tugas2.py
@@ -1,32 +1,30 @@
 
 #menghitung luas persegi panjang
-panjang = 15
-lebar = 18
+panjang = float(input("masukkan nilai panjang"))
+lebar = float(input("masukkan nilai lebar"))
 luas_persegipanjang = panjang * lebar
-print("panjang: ", panjang)
-print("lebar: ", lebar)
-print("luas persegi panjang: ", luas_persegipanjang)
+print("panjang = ", panjang)
+print("lebar = ", lebar)
+print("luas persegi panjang = ", luas_persegipanjang)
 
 #menghitung luas lingkaran
-r = float(14)
+r = float(input("Masukan nilai jari-jari"))
 luas_lingkaran = 3,14*(r ** 2)
-print("r: ", r)
-print("luas lingkaran: ", luas_lingkaran)
+print("r= ", r)
+print("luas lingkaran = ", luas_lingkaran)
 
 #menghitung luas kubus
-s = 10
+s = float(input("Masukan nilai panjang sisi"))
 luas_permukaan_kubus = 6 * (s ** 2)
-print("s: ", s)
-print("luas permukaan kubus: ", luas_permukaan_kubus)
+print("s = ", s)
+print("luas permukaan kubus = ", luas_permukaan_kubus)
 
 #menghitung koversi suhu celcius ke farenheit
-C = 65
+C = float(input("Masukkan suhu celcius"))
 F = ((9/5) * C) + 32
-print("C: ", C)
-print("F: ", F)
+print("Hasil konversi", C, "celcius =", F, "farenheit")
 
-#menghitung suhu reamur ke kelvin
-R = 48
+#menghitung konversi suhu reamur ke kelvin
+R = float(input("Masukkan suhu reamur"))
 K = ((5/4) * R) + 273
-print("R: ", R)
-print("K: ", K)
+print("Hasil konversi", R, "reamur =", K, "kelvin")
